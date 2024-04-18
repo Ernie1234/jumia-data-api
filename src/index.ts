c9f2578d179fff54da16f13a4b2ef36500baa237
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-04-18 13:33:51 +0100

    routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-import request from "request";
-import axios from "axios";
+import puppeteer from "puppeteer";
+
+import router from "./routes/products";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -13,54 +14,18 @@ app.use(
   })
 );
 
-const API_KEY = "6dc1072528e57eb4c506276efbaa3efb";
-const BASE_URL = `http://api.scraperapi.com?api_key=${API_KEY}&autoparse=true`;
+// using puppeteer to scrape jumia
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.jumia.com/");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-//GET PRODUCT DETAILS
-app.get("/products/:productId", async (req, res) => {
-  const { productId } = req.params;
+  await browser.close();
 
-  const site = "https://www.jumia.com.ng/";
-  const url = `${BASE_URL}&url=${site}/${productId}`;
-
-  try {
-    const resp = await axios(url);
-    res.send(resp.data);
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-// GET RATING LIST OF VERIFIED USERS
-app.get("/catalog/:productId", async (req, res) => {
-  const { productId } = req.params;
-
-  const site = "https://www.jumia.com.ng/sku";
-  const url = `${BASE_URL}&url=${site}/${productId}`;
-
-  try {
-    const resp = await axios(url);
-    res.send(resp.data);
-  } catch (error) {
-    res.json(error);
-  }
-});
-app.get("/search/:searchId", async (req, res) => {
-  const { searchId } = req.params;
-
-  const site = "https://www.jumia.com.ng/catalog";
-  const url = `${BASE_URL}&url=${site}/?q=${searchId}`;
-
-  try {
-    const resp = await axios(url);
-    res.send(resp.data);
-  } catch (error) {
-    res.json(error);
-  }
-});
+  app.use("/", router);
+})();
 
 app.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}/`);
---
+++ src/routes/products.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+import { getAllProducts } from "../controllers/products";
+
+const router = express.Router();
+
+router.get("/products", getAllProducts);
+
+export default router;
